1e57e6c5c9186b199c39d5f6a6a513951b8a0e58
Hyaku Ju Yon <ichitora114@outlook.com>
2021-03-12 04:27:31 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _4._1_Mod_Pract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int A = 4;
+
+            int B = 6;
+
+            bool isAEqualB = A == B;
+
+            Console.WriteLine("A is equal B > {0}", isAEqualB);
+
+            Console.ReadKey();
+        }
+    }
+}
