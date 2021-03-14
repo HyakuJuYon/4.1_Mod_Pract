3c4ae7faa93274fed7358c5d2faf17e77cb2f432
Hyaku Ju Yon <ichitora114@outlook.com>
2021-03-14 04:32:53 +0300

    Changes_6

--- Program.cs
+++ Program.cs
@@ -72,6 +72,40 @@ namespace _4._1_Mod_Pract
                 Console.WriteLine("The statement is false");
             }
 
+            Console.WriteLine("Write down your favourite color in English using small letters >");
+
+            var color = Console.ReadLine();
+
+            if (color == "red")
+            
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is red!");
+            }
+
+            else if (color == "green")
+            
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is green!");
+            }
+            
+            else
+            
+            {
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is cyan!");
+            }
+
             Console.ReadKey();
 
         }
